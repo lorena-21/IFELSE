3408f448a8acc435c4a79631834d14fe5093eb4a
lorena-21 <110685156+lorena-21@users.noreply.github.com>
2022-11-21 14:28:26 -0500

    Initial commit

---
+++ src/steemit/ClaseSteemit.java
@@ -0,0 +1,18 @@
+package steemit;
+
+public class ClaseSteemit {
+
+    public static void main(String[] args) {
+        int primerNumero = 8, segundoNumero = 10;
+
+        if (primerNumero > segundoNumero) {
+            System.out.println("el primero " + primerNumero + " es mayor");
+        } else
+            System.out.println("el segundo numero " + segundoNumero + " es mayor");
+    }
+}
+
+
+
+
+
